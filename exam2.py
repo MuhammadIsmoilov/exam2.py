c0b487d7caa83ada1fd8e8f11c4b275292adbabc
MuhammadIsomoilov <ismoilovmuhammad247.com>
2024-01-27 20:24:44 +0500

    first commit

---
+++ exam2.py
@@ -0,0 +1,28 @@
+#  2.# a = input().split()
+# maxx = -99999
+# for i in a:
+#      if a == '0':
+#         break
+#      elif int(i) > int(maxx):
+#         maxx = i
+   
+# print(maxx) 
+#  3.# n = int (input())
+# i = 1
+# while i*i <= n:
+#     print(i*i,end=" ")
+#     i += 1  
+#  4.list1 = [7,6,5,4,3,2,1]
+#    list1.pop(2)
+#    print(list1)
+# 6
+list1 = input().split()
+print(list1[-1],list1[0],list1[1],list1[2],list1[3])
+
+
+
+
+
+
+
+     
